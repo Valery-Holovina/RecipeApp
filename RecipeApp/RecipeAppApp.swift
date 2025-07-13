31c24af3eb8d54e455ba3aa4bf57684eadde29d6
Valery-Holovina <valery.holovina09@gmail.com>
2025-07-13 13:47:51 +0300

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Valery on 13.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
